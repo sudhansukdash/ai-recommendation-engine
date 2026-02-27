d0e93e6590b5e9cf431d161d3dd24483e06b6419
Sudhansu Kumar Dash <sudhansukumar.dash101@gmail.com>
2026-02-27 09:50:57 +0530

    Renamed and restructured logic blocks in pipeline
    - Renamed 07_clean_interactions_data to 07_process_interactions.
    - Moved the groupby and clipping logic from 08_item_user_matrix
    to 07_process_interactions.
    - Fixed minor naming inconsistencies across files.

--- pipeline/07_process_interactions.py
+++ pipeline/07_process_interactions.py
@@ -7,7 +7,7 @@ import os
 #Return the project root folder name
 PROJECT_ROOT = os.path.basename(os.getcwd())
 
-print("\nStarting interaction data cleaning...")
+print("\n1. Starting interaction data cleaning...")
 
 #Reading the interactions_raw.csv and storing it in a DataFrame
 raw_interactions = pd.read_csv("data/raw/interactions_raw.csv")
@@ -24,16 +24,18 @@ clean_interactions["interaction_type"] = clean_interactions["interaction_type"].
 #It groups the duplicate ['user_id', 'pr_id', 'interaction_type'] in a single row and counts occurances of such rows (.size()) and creates a new column named frequency where the count is stored. Here only completely same rows are grouped for ex U1,P1, view has been grouped but u1,p1,purchase might exist which we will handle in user_item_matrix
 #It retains if user have bought and item 2 times insteading of deleting such interactions which would have a stronger recommendation.
 #Now the clean_interactions have fields user_id, product_id, interaction_type, frequency
+
+print("2. Grouping identical rows...")
 clean_interactions = clean_interactions.groupby(['user_id', 'pr_id', 'interaction_type']).size().reset_index(name='frequency')
 # print(clean_interactions.value_counts(subset="interaction_type"))
 
+print("3. Converting interactions_type to float...")
 #After cleaning interactions we are using a weight map to replace the strings in interaction_type to actual values that model understand
 weight_map = {
-    "view" : 0.5,
+    "view": 0.5,
     "wishlist": 3,
     "cart": 5,
-    "purchase" : 30
-
+    "purchase": 30
 }
 
 #Create a new col. weight and use the weight_map and interaction_type col from df to add values accordingly here
@@ -46,14 +48,26 @@ clean_interactions["weight"] = clean_interactions["weight"].fillna(0)
 clean_interactions["scores"] = clean_interactions["weight"] * clean_interactions["frequency"]
 # Remove meaningless interactions (score 0)
 clean_interactions = clean_interactions[clean_interactions['scores'] > 0]
+
+print("4. Dropping redundant columns...")
 #Dropping the col. interaction_type,weight as it no more needed 
 clean_interactions = clean_interactions.drop(columns=["interaction_type","weight"])
 # print(clean_interactions.head())
 
-print(clean_interactions.info())
-print("\nInteraction data cleaned successfully!")
+#Similar user_id, pr_id pairs may exists, so we are using group-by to combine all those pairs and view total_scores
+#We are using sum to consider all scores like viewing, buying multiple times etc. to prevent a user viewing an item 10 times and overpowering someone who have bought it we increase the purchase weight to 30
+#Grouping user_id and pr_id and converting multi-index as single index by using as_index=False, adding the columns scores and renaming it as total_scores
+grouped_interactions = clean_interactions.groupby(["user_id", "pr_id"], as_index=False).agg(total_scores=("scores", "sum"))
+# print(grouped_interactions.head())
 
-#Saving this clean_interactions dataframe into a new interactions_clean.csv file
-clean_interactions.to_csv("data/processed/interactions_clean.csv", index=False)
+print("5. Clipping scores...")
+#Max we are keeping the upper range for total_scores 70 for each user-product pair
+grouped_interactions["total_scores"] = grouped_interactions["total_scores"].clip(upper=70)
 
-print(f"File saved as interactions_clean.csv under {PROJECT_ROOT}/data/processed/")
+print("\nInteraction data remodelled successfully!")
+print(grouped_interactions.info())
+
+#Saving this clean_interactions dataframe into a new interactions_final.csv file
+grouped_interactions.to_csv("data/processed/interactions_final.csv", index=False)
+
+print(f"File saved as interactions_final.csv under {PROJECT_ROOT}/data/processed/")
